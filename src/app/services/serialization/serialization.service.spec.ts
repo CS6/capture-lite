de2206a5361459df380717848e8ad92730307a76
ethanwu155 <et.wuzz@gmail.com>
2020-08-07 11:52:38 +0800

    removed extra console log

--- src/app/services/serialization/serialization.service.spec.ts
+++ src/app/services/serialization/serialization.service.spec.ts
@@ -29,7 +29,6 @@ describe('SerializationService', () => {
     const expectedOutput = '{"proof":{"hash":"127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935","mimeType":{"type":"picture","extension":"jpeg"},"timestamp":20201208},"sortedInformation":[]}';
 
     service.stringify$(newProof).subscribe(x => {
-      console.log('stringify', x);
       expect(x).toEqual(expectedOutput);
     });
   });
