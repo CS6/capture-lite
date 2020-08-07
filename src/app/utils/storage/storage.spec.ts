8f4393defead5cdab25b7bfcf19ec1ea068481ee
ethanwu155 <et.wuzz@gmail.com>
2020-08-07 11:06:28 +0800

    added util/storage unit test

--- src/app/utils/storage/storage.spec.ts
+++ src/app/utils/storage/storage.spec.ts
@@ -1,1 +1,27 @@
-describe('storage', () => { });
+import { Storage } from 'src/app/utils/storage/storage';
+
+describe('storage', () => {
+
+    let storage: Storage<object>;
+    let option: [string, boolean];
+
+    beforeEach(() => {
+        storage = new Storage('test');
+        option = ['test', true];
+    });
+
+    it('test add$', (done: DoneFn) => {
+        storage.add$(option).subscribe(x => {
+            console.log('test', x);
+            expect(x).toEqual([['test', true]]);
+            done();
+        });
+    });
+
+    it('test remove', (done: DoneFn) => {
+        storage.remove$(option).subscribe(x => {
+            expect(x).toEqual([['test', true]]);
+            done();
+        });
+    });
+});
