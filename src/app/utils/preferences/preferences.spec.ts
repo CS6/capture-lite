d59eca2590fd8b9d05151ed33624325e986e72fe
ethanwu155 <et.wuzz@gmail.com>
2020-08-07 11:08:04 +0800

    added preferences util unit test

--- src/app/utils/preferences/preferences.spec.ts
+++ src/app/utils/preferences/preferences.spec.ts
@@ -1,1 +1,39 @@
-describe('preferences', () => { });
+import { Preferences } from './preferences';
+
+describe('preferences', () => {
+
+    let preferences: Preferences;
+
+    beforeEach(() => {
+        preferences = new Preferences('testKey');
+    });
+
+
+    it('test setNumber$ & getNumber$', (done: DoneFn) => {
+        const value = 123;
+        preferences.setNumber$('testKey', value).subscribe(x => {
+            console.log('setNumber', x);
+            expect(x).toEqual(value);
+            done();
+        });
+        preferences.getNumber$('testKey').subscribe(y => {
+            console.log('getNumber', y);
+            expect(y).toEqual(value);
+            done();
+        });
+    });
+
+    it('test setString$ & getString$', (done: DoneFn) => {
+        const value = 'hello';
+        preferences.setString$('testKey', value).subscribe(x => {
+            console.log('setString', x);
+            expect(x).toEqual(value);
+            done();
+        });
+        preferences.getString$('testKey', value).subscribe(y => {
+            console.log('getString', y);
+            expect(y).toEqual(value);
+            done();
+        });
+    });
+});
